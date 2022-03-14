dc08b1f6fe30100a3e73c904b01ad228d316c500
AAverin <a.a.averin@gmail.com>
2022-03-14 12:10:23 +0100

    Make sure everything is commited after generation. Change commit messages to have more context.

--- engine/library/1-ci/1-publishing/0-maven/readme.md
+++ engine/library/1-ci/1-publishing/0-maven/readme.md
@@ -1,3 +1,4 @@
 ## Maven Publishing
 
 Don't forget to provide maven url and credentials
+
--- engine/src/commandsProcessor.js
+++ engine/src/commandsProcessor.js
@@ -1,23 +1,23 @@
-const fs = require("fs")
-const fsp = require("fs/promises")
+const fs = require("fs");
+const fsp = require("fs/promises");
 
 const processFolder = (props) => {
-  const { shell, folder, config, logger } = props
-  var result = []
+  const { shell, folder, config, logger } = props;
+  var result = [];
   if (!fs.statSync(folder).isDirectory()) {
-    logger.log(folder, "is not a directory")
-    return result
+    logger.log(folder, "is not a directory");
+    return result;
   }
-  logger.logVerbose("processFolder", folder)
-  shell.cd(folder)
-  const files = shell.ls()
+  logger.logVerbose("processFolder", folder);
+  shell.cd(folder);
+  const files = shell.ls();
   if (files.includes("rule.js")) {
-    logger.logVerbose("Reading rule in", folder)
-    const rule = require(`${shell.pwd()}/rule.js`)
+    logger.logVerbose("Reading rule in", folder);
+    const rule = require(`${shell.pwd()}/rule.js`);
     if (!rule.shouldRun(config, logger)) {
-      logger.logVerbose("Ignoring due to rule in", folder)
-      shell.cd("..")
-      return result
+      logger.logVerbose("Ignoring due to rule in", folder);
+      shell.cd("..");
+      return result;
     }
   }
 
@@ -25,44 +25,50 @@ const processFolder = (props) => {
     result = processPatches({
       shell: shell,
       logger: logger,
-    })
+    });
   } else {
     files.forEach((sub) => {
       const subResult = processFolder({
         ...props,
         folder: sub,
-      })
-      result = [...result, ...subResult]
-    })
+      });
+      result = [...result, ...subResult];
+    });
   }
 
-  shell.cd("..")
-  return result
-}
+  shell.cd("..");
+  return result;
+};
 
 const processPatches = ({ shell, logger }) => {
-  const commands = []
-  logger.logVerbose("processPatches")
-  const files = shell.ls()
+  const commands = [];
+  logger.logVerbose("processPatches");
+  const files = shell.ls();
   if (files.includes("staged.diff")) {
-    commands.push(`git apply --ignore-space-change --ignore-whitespace ${shell.pwd()}/staged.diff`)
+    commands.push(
+      `git apply --ignore-space-change --ignore-whitespace ${shell.pwd()}/staged.diff`
+    );
   }
   if (files.includes("patch.diff")) {
-    commands.push(`git apply --ignore-space-change --ignore-whitespace ${shell.pwd()}/patch.diff`)
+    commands.push(
+      `git apply --ignore-space-change --ignore-whitespace ${shell.pwd()}/patch.diff`
+    );
   }
   if (files.includes("run.sh")) {
-    commands.push(`bash ${shell.pwd()}/run.sh"`)
-  }
-  commands.push(`git add -A`)
-  if (files.includes("commit.msg")) {
-    commands.push(`git commit -m "${shell.cat("commit.msg")}"`)
-  } else {
-    commands.push(`git commit -m "Create-Android-Project Library: Applying patch"`)
+    commands.push(`bash ${shell.pwd()}/run.sh"`);
   }
   if (files.includes("readme.md")) {
-    commands.push(`cat ${shell.pwd()}/readme.md >> ./readme.md`)
+    commands.push(`cat ${shell.pwd()}/readme.md >> ./readme.md`);
   }
-  return commands
-}
+  commands.push(`git add -A`);
+  if (files.includes("commit.msg")) {
+    commands.push(`git commit -m "${shell.cat("commit.msg")}"`);
+  } else {
+    commands.push(
+      `git commit -m "Create-Android-Project Library: Applying patch"`
+    );
+  }
+  return commands;
+};
 
-module.exports.process = processFolder
+module.exports.process = processFolder;
